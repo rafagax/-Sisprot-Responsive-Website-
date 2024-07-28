3ff7f7551114dc3f1144ac37b20b672aba1a2bc8
Jesus Vasquez <inversionesdigitales@hotmail.es>
2024-07-28 17:43:53 -0400

    .

--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -3,12 +3,16 @@ import { Link } from 'react-router-dom'
 import Layout from '../../Components/Layout'
 import { ShoppingCartContext } from '../../Context'
 import OrdersCard from '../../Components/OrdersCard'
+import DropdownMenu from '../../Components/DropdownMenu'
 
 function MyOrders() {
   const context = useContext(ShoppingCartContext)
 
   return (
+    <>
+    
     <Layout>
+   
       <div className='flex items-center justify-center relative w-80 mb-4'>
         <h1 className='font-medium text-xl'>My Orders</h1>
       </div>
@@ -22,6 +26,7 @@ function MyOrders() {
         ))
       }
     </Layout>
+    </>
   )
 }
 
