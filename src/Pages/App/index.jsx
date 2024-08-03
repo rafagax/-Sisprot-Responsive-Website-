920b1fa49b84571406e400ae2ecd124c87dda08e
Jesus Vasquez <inversionesdigitales@hotmail.es>
2024-08-03 13:07:42 -0400

    Revert "Delete src/Pages/App/index.jsx"
    This reverts commit aa1007d979af2dfa2792ff3c5c05e2b1fe933eac.

---
+++ src/Pages/App/index.jsx
@@ -0,0 +1,39 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom';
+
+import Instalacion from '../Instalacion'; // Asegúrate de que la ruta sea correcta
+import NotFound from '../NotFound';
+import Navbar from '../../Components/Navbar';
+import MisOrdenes from '../MisOrdenes';
+import SisprotTv from '../SisprotTv';
+import Otros from '../Otros';
+
+import './App.css';
+import PlanesYServicios from '../PlanesyServicios';
+
+
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <PlanesYServicios /> },
+    { path: '/PlanesyServicios', element: <PlanesYServicios /> },
+    { path: '/SisprotTv', element: <SisprotTv /> },
+    { path: '/Instalacion', element: <Instalacion /> },
+    { path: '/Otros', element: <Otros /> },
+    { path: '/MisOrdenes', element: <MisOrdenes /> },
+    { path: '/*', element: <NotFound /> },
+  ]);
+
+  return routes;
+};
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      
+      <AppRoutes />
+    </BrowserRouter>
+  );
+};
+
+export default App;
