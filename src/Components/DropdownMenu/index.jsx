da8948821e693e145362e928e225b8c1481f3638
Jesus Vasquez <inversionesdigitales@hotmail.es>
2024-08-03 14:49:51 -0400

    /

--- src/Components/DropdownMenu/index.jsx
+++ src/Components/DropdownMenu/index.jsx
@@ -20,11 +20,10 @@ const DropdownMenu = () => {
         Menu
       </button>
       <ul className='dropdown-content'>
-        <li><NavLink to='/PlanesYServicios' className='dropdown-link'>Planes y Servicios</NavLink></li>
+      <li><NavLink to='/Otros' className='dropdown-link'>Planes y Servicios</NavLink></li>
         <li><NavLink to='/SisprotTv' className='dropdown-link'>Sisprot Tv</NavLink></li>
         <li><NavLink to='/Instalacion' className='dropdown-link'>Instalacion</NavLink></li>
-        <li><NavLink to='/Otros' className='dropdown-link'>Otros</NavLink></li>
-        <li><NavLink to='/MisOrdenes' className='dropdown-link'>Mis Ordenes</NavLink></li>
+       <li><NavLink to='/MisOrdenes' className='dropdown-link'>Mis Ordenes</NavLink></li>
       </ul>
     </div>
   );
